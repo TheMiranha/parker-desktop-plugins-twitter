0cd4f20574ff26bb7b58b30e03a615bd1102e667
TheMiranha <lxbrmiranda@gmail.com>
2022-05-30 01:54:21 -0300

    first commit

---
+++ home/sidebar.js
@@ -0,0 +1,10 @@
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const Item = () => {
+    return {
+        title: "Twitter",
+        icon: <TwitterIcon style={{marginRight: 10}}/>
+    }
+}
+
+export default { Item }
